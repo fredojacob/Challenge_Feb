8db0fdcaa8ad5a5c0176d2790f0fca48a212030e
Freddy Gonzalez <fredo_y@hotmail.com>
2021-02-12 21:32:58 -0600

    model complaits

---
+++ app/Http/Controllers/ComplaintsController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\complaints;
+use Illuminate\Http\Request;
+
+class ComplaintsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\complaints  $complaints
+     * @return \Illuminate\Http\Response
+     */
+    public function show(complaints $complaints)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\complaints  $complaints
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(complaints $complaints)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\complaints  $complaints
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, complaints $complaints)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\complaints  $complaints
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(complaints $complaints)
+    {
+        //
+    }
+}
