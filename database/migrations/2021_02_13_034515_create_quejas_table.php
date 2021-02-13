74fa1ce8afe0b28a7b51d44722cd08b48eec7178
Freddy Gonzalez <fredo_y@hotmail.com>
2021-02-12 21:50:22 -0600

    modelo quejas

---
+++ database/migrations/2021_02_13_034515_create_quejas_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateQuejasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('quejas', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('quejas');
+    }
+}
